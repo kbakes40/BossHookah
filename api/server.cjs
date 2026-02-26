898d323275b892d1da0233143d5c1db430d0d708
kbakes40 <kevin@bakerhub.com>
2026-02-26 15:26:38 -0500

    cleanup: remove placeholder server.cjs from api/

--- api/server.cjs
+++
@@ -1,3 +0,0 @@
-// This file is replaced during build by esbuild
-// Placeholder to satisfy Vercel functions config validation
-module.exports = function handler(req, res) { res.status(503).send('Building...'); };
